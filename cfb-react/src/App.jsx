6d8d06406d6cca74619f851bf00bb5cd1e412a12
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-20 09:12:22 -0300

    Aula 18 children 1.1

--- cfb-react/src/App.jsx
+++ cfb-react/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
-import Caixa from './components/Caixa'
-//import Canal from './components/Canal'
+
 
 function App() {
 
   return(
 
     <>
-      <Caixa site = "www.ff.ff.com">
-        <h1>CFB Cursos</h1>
-        <p>Canal no Youtube</p>
+      <h1>CFB Cursos</h1>
 
-      </Caixa>
+
     </>
   )
   
